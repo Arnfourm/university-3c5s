8b1dd503135aaf689407f01e87631c71efa791ab
luver <arnfourm@gmail.com>
2025-11-17 23:00:10 +0300

    Lab 4 example done, my own task left

---
+++ tp/lab-4/Example/Example/TestingClass.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Legacy;
+
+namespace Example
+{
+    [TestFixture]
+    public class TestingClass
+    {
+        [Test]  
+        public void TestGetDayOfWeekName()
+        {
+            ClassicAssert.AreEqual("Понедельник", WeekDayConverter.GetNameByDay(1));
+            ClassicAssert.AreEqual("Вторник", WeekDayConverter.GetNameByDay(2));
+            ClassicAssert.AreEqual("Среда", WeekDayConverter.GetNameByDay(3));
+            ClassicAssert.AreEqual("Четверг", WeekDayConverter.GetNameByDay(4));
+            ClassicAssert.AreEqual("Пятница", WeekDayConverter.GetNameByDay(5));
+            ClassicAssert.AreEqual("Суббота", WeekDayConverter.GetNameByDay(6));
+            ClassicAssert.AreEqual("Воскресенье", WeekDayConverter.GetNameByDay(7));
+        }
+
+        [Test]
+        public void TestGetDayOfWeekNameInvalidArgument()
+        {
+            Assert.That(() => WeekDayConverter.GetNameByDay(8),
+                Throws.TypeOf<ArgumentException>());
+        }
+
+    }
+}
